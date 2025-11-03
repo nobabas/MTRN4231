3313149a7c1bfaef654fe1eee12c8ff65333b6bb
nobabas <brent.poon1@gmail.com>
2025-11-03 12:32:42 +1100

    more commiting

---
+++ ros2_ws/src/interface/build/blue_detector_msgs/CMakeFiles/blue_detector_msgs__rosidl_typesupport_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for blue_detector_msgs__rosidl_typesupport_cpp.
+# This may be replaced when dependencies are built.
---
+++ ros2_ws/src/interface/build/blue_detector_msgs/CMakeFiles/blue_detector_msgs__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blue_detector_msgs__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/blue_detector_msgs/msg/detail/dds_fastrtps/marker_data__type_support.cpp.o"
+  "CMakeFiles/blue_detector_msgs__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/blue_detector_msgs/msg/detail/dds_fastrtps/marker_data__type_support.cpp.o.d"
+  "libblue_detector_msgs__rosidl_typesupport_fastrtps_cpp.pdb"
+  "libblue_detector_msgs__rosidl_typesupport_fastrtps_cpp.so"
+  "rosidl_typesupport_fastrtps_cpp/blue_detector_msgs/msg/detail/dds_fastrtps/marker_data__type_support.cpp"
+  "rosidl_typesupport_fastrtps_cpp/blue_detector_msgs/msg/detail/marker_data__rosidl_typesupport_fastrtps_cpp.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blue_detector_msgs__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
